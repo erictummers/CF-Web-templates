09a1638783197a21822f6bd505ccec21b1697ed3
Eric Tummers <erictummers@me.com>
2019-10-14 14:17:58 +0200

    Add database migration commandline

--- EF-codefirst-migration/Program.cs
+++ EF-codefirst-migration/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using EF_codefirst_migration.Context;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 
@@ -15,7 +18,23 @@ namespace EF_codefirst_migration
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            switch (args[0])
+            {
+                // cf run-task EFCodeFirstMigration "cd /home/vcap/deps/0/dotnet_publish && exec ./EF-codefirst-migration --update-database" --name update-database-task
+                case "--update-database":
+                    // start task and print to console
+                    Console.WriteLine("Getting database ...");
+                    var host = CreateWebHostBuilder(args).Build();
+                    var context = host.Services.GetService<SchoolContext>();
+                    Console.WriteLine("Updating database ...");
+                    context.Database.Migrate();
+                    Console.WriteLine("Update database done");
+                    break;
+                default:
+                    // normal start from web application
+                    CreateWebHostBuilder(args).Build().Run();
+                    break;
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
